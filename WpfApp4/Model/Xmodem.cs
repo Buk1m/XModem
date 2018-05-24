6a5b98b78c561c6cc804b8c631b6d6cf426c9a9d
Buk1m <bkudra@hotmail.com>
2018-05-24 09:34:15 +0200

    initial commit

---
+++ WpfApp4/Model/Xmodem.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Reflection;
+using System.Windows;
+
+namespace WpfApp4.Model
+{
+    public class Xmodem
+    {
+        private const int SOH = 1;
+        private const int EOT = 4;
+        private const int ACK = 6;
+        private const int NAK = 21;
+        private const int SUB = 26;
+
+        private readonly SerialPort portNumber;
+
+        public string Message { get; set; }
+
+        public Xmodem( string portName, int baudrate )
+        {
+            portNumber = new SerialPort( portName ) {ReadTimeout = 10000, BaudRate = baudrate};
+            portNumber.Open();
+        }
+
+        public void Close()
+        {
+            portNumber.Close();
+        }
+
+        public void Send( byte[] bytesToSend )
+        {
+            try
+            {
+                if (!portNumber.IsOpen)
+                    portNumber.Open();
+                using (portNumber)
+                {
+                    while (Convert.ToInt32( portNumber.ReadLine() ) != NAK) ;
+
+                    for (int i = 0; i <= bytesToSend.Length / 128; i++)
+                    {
+                        do
+                        {
+                            portNumber.WriteLine( SOH.ToString() );
+                            portNumber.WriteLine( i.ToString() );
+                            portNumber.WriteLine( ( 255 - i ).ToString() );
+                            for (int j = 0; j < 128; j++)
+                            {
+                                if (i * 128 + j == bytesToSend.Length)
+                                {
+                                    portNumber.Write( Convert.ToChar( SUB ).ToString() );
+                                    break;
+                                }
+
+                                portNumber.Write( Convert.ToChar( bytesToSend[i * 128 + j] ).ToString() );
+
+                            }
+
+                            byte[] crc = createCRC( bytesToSend );
+                            for (int j = 0; j < 2; j++)
+                            {
+                                portNumber.Write( Convert.ToChar( crc[j] ).ToString() );
+                            }
+
+                        } while (Convert.ToInt32( portNumber.ReadLine() ) == NAK);
+                    }
+
+                    do
+                    {
+                        portNumber.WriteLine( EOT.ToString() );
+                    } while (Convert.ToInt32( portNumber.ReadLine() ) != ACK);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is InvalidOperationException || ex is TimeoutException || ex is IOException)
+                {
+                    MessageBox.Show( ex.Message );
+                    return;
+                }
+                throw;
+            }
+        }
+
+        public void Receive()
+        {
+            try
+            {
+                if (!portNumber.IsOpen)
+                    portNumber.Open();
+                using (portNumber)
+                {
+                    List<byte> tmpByteList = new List<byte>();
+                    List<byte> byteList = new List<byte>();
+
+                    do
+                    {
+                        portNumber.WriteLine( NAK.ToString() );
+                    } while (Convert.ToInt32( portNumber.ReadLine() ) != SOH);
+
+                    bool error = false;
+                    do
+                    {
+                        if (255 - Convert.ToInt32( portNumber.ReadLine() ) != Convert.ToInt32( portNumber.ReadLine() ))
+                            error = true;
+                        else
+                        {
+                            for (int i = 0; i < 128; i++)
+                            {
+                                int tmp = portNumber.ReadChar();
+                                if (tmp == SUB)
+                                    break;
+                                else
+                                    tmpByteList.Add( Convert.ToByte( tmp ) );
+                            }
+
+                            bool ifDataCorrect = true;
+                            byte[] crc = createCRC( tmpByteList.ToArray() );
+                            for (int j = 0; j < 2; j++)
+                            {
+                                if (crc[j] != Convert.ToByte( portNumber.ReadChar() ))
+                                    ifDataCorrect = false;
+                            }
+
+                            if (ifDataCorrect)
+                                error = true;
+                            else
+                            {
+                                foreach (var item in tmpByteList)
+                                {
+                                    byteList.Add( item );
+                                }
+
+                                tmpByteList.Clear();
+                                portNumber.WriteLine( ACK.ToString() );
+                            }
+                        }
+
+                        if (error)
+                        {
+                            portNumber.DiscardInBuffer();
+                            portNumber.WriteLine( NAK.ToString() );
+                        }
+
+                    } while (portNumber.ReadLine() == SOH.ToString());
+
+                    portNumber.WriteLine( ACK.ToString() );
+
+                    byte[] byteArray = byteList.ToArray();
+                    File.WriteAllBytes( @"..\..\message.txt", byteArray );
+                }
+            }
+            catch (TimeoutException te)
+            {
+                MessageBox.Show( te.Message );
+            }
+        }
+
+        private static byte[] createCRC( byte[] package )
+        {
+            int crc = 0;
+            foreach ( byte item in package )
+            {
+                crc = crc ^ item << 8;
+                for ( int i = 0; i < 8; i++ )
+                {
+                    if ( Convert.ToBoolean( crc & 0x8000 ) )
+                        crc = crc << 1 ^ 0x1021;
+                    else
+                        crc = crc << 1;
+                }
+            }
+
+            crc = crc & 0xFFFF;
+
+            return BitConverter.GetBytes( crc );
+        }
+    }
+}
---
+++ WpfApp4/ViewModel/MainViewModel.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using WpfApp4.Commands;
+using WpfApp4.Model;
+
+namespace WpfApp4.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Private Members
+
+        private Xmodem _senderModem;
+        private Xmodem _reciverModem;
+        private string _selectedSenderCom;
+        private string _selectedReciverCom;
+        private string _senderMessage;
+        private string _reciverMessage;
+        private int _selectedSenderBaudrate = 9600;
+        private int _selectedReciverBaudrate = 9600;
+
+        #endregion
+
+        #region Constructor
+
+        public MainViewModel()
+        {
+            OpenFile = new RelayCommand( () =>
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog {Filter = "Text files (*.txt)|*.txt"};
+                if (openFileDialog.ShowDialog() != true)
+                    return;
+
+                SenderTextBox = File.ReadAllText( openFileDialog.FileName );
+            } );
+
+            Send = new RelayCommand( () =>
+            {
+                if (_senderModem == null || _selectedSenderCom == null)
+                {
+                    MessageBox.Show( "Choose serial port." );
+                    return;
+                }
+
+                if (SenderTextBox == null)
+                {
+                    MessageBox.Show( "Enter message to be sent." );
+                    return;
+                }
+
+                Task.Run( () => _senderModem.Send( Encoding.ASCII.GetBytes( SenderTextBox + "\r\n" ) ) );
+            } );
+
+            BackgroundWorker reciveWorker = new BackgroundWorker();
+            reciveWorker.DoWork += ReciveMessage;
+            reciveWorker.RunWorkerCompleted += ReciveWorker_RunWorkerCompleted;
+
+            Receive = new RelayCommand( () =>
+            {
+                if ( _reciverModem == null )
+                {
+                    MessageBox.Show( "Choose serial port." );
+                    return;
+                }
+
+                if ( !reciveWorker.IsBusy)
+                    reciveWorker.RunWorkerAsync();
+            } );
+
+            RefreshPorts = new RelayCommand( () =>
+            {
+                _senderModem?.Close();
+                _reciverModem?.Close();
+                _selectedReciverCom = null;
+                _selectedSenderCom = null;
+                ComPorts?.Clear();
+                foreach (var portName in SerialPort.GetPortNames())
+                {
+                    ComPorts?.Add( portName );
+                }
+            } );
+        }
+
+        private void ReciveWorker_RunWorkerCompleted( object sender, RunWorkerCompletedEventArgs e )
+        {
+            ReciverTextBox = File.ReadAllText( @"..\..\message.txt" );
+        }
+
+        public void ReciveMessage( object sender, EventArgs e )
+        {
+            Task a = Task.Run(() => _reciverModem.Receive());
+            a.Wait();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public ObservableCollection<string> ComPorts { get; set; } =
+            new ObservableCollection<string>( SerialPort.GetPortNames() );
+
+        public ObservableCollection<string> Baudrate { get; set; } =
+            new ObservableCollection<string>() {"9600", "115200"};
+
+        public string SelectedSenderBaudrate
+        {
+            get => _selectedSenderBaudrate.ToString();
+            set
+            {
+                _selectedSenderBaudrate = int.Parse( value );
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedReciverBaudrate
+        {
+            get => _selectedReciverBaudrate.ToString();
+            set
+            {
+                _selectedReciverBaudrate = int.Parse( value );
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedSenderCom
+        {
+            get => _selectedSenderCom;
+            set
+            {
+                if (_selectedSenderCom == value || value == null )
+                    return;
+
+                _selectedSenderCom = value;
+                try
+                {
+                    _senderModem?.Close();
+                    _senderModem = new Xmodem( _selectedSenderCom, _selectedSenderBaudrate );
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show( "Port is being used by other process. Acces denied." );
+                    OnPropertyChanged();
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedReciverCom
+        {
+            get => _selectedReciverCom;
+            set
+            {
+                if (_selectedReciverCom == value || value == null)
+                    return;
+                _selectedReciverCom = value;
+                try
+                {
+                    _reciverModem?.Close();
+                    _reciverModem = new Xmodem( _selectedReciverCom, _selectedReciverBaudrate );
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show( "Port is being used by other process. Acces denied" );
+                    OnPropertyChanged();
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        public string SenderTextBox
+        {
+            get => _senderMessage;
+
+            set
+            {
+                _senderMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ReciverTextBox
+        {
+            get => _reciverMessage;
+
+            set
+            {
+                _reciverMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand OpenFile { get; set; }
+        public ICommand Send { get; set; }
+        public ICommand Receive { get; set; }
+        public ICommand RefreshPorts { get; set; }
+
+        #endregion
+    }
+}
